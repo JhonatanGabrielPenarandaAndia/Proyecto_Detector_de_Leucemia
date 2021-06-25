6b11e29fc10192e8344afde231386b73ccf36d27
pablo rodriguez <pablors0598@gmail.com>
2021-06-25 15:20:25 -0400

    agregando los archivos base del patron de diseño, cambio la ventana de inicio por una de la vista

---
+++ Proyecto_Detector_de_Leucemia/ViewModels/ViewModel_Main.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Detector_de_Leucemia.ViewModels
+{
+    public class ViewModel_Main:IGeneric
+    {
+    }
+}
