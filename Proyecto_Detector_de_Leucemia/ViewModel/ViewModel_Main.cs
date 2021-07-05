fb65df30755261d3e544a0354ebc92b869d3eda2
JhonatanGabrielPenarandaAndia <gabriel24332@hotmail.com>
2021-07-04 23:49:28 -0400

    Implementacion de metodos para procesamiento de imagenes y Comandos

--- Proyecto_Detector_de_Leucemia/ViewModel/ViewModel_Main.cs
+++ Proyecto_Detector_de_Leucemia/ViewModel/ViewModel_Main.cs
@@ -11,6 +11,10 @@ using System.Windows.Input;
 using System.Windows.Media.Imaging;
 using AForge.Imaging.Filters;
 using AForge.Imaging;
+using AForge.Imaging.ColorReduction;
+using AForge.Math.Geometry;
+using AForge;
+using System.Collections.Generic;
 
 namespace Proyecto_Detector_de_Leucemia.ViewModel
 {
@@ -21,12 +25,7 @@ namespace Proyecto_Detector_de_Leucemia.ViewModel
         private BitmapImage croppedImage;
         private BitmapImage imageToShow;
         private Model_CroppedImage imageSelected;
-
-        public Model_CroppedImage ImageSelected
-        {
-            get { return imageSelected; }
-            set { imageSelected = value; OnPropertyChanged(); }
-        }
+        private Bitmap imageMainBitmap;
 
         private ObservableCollection<Model_CroppedImage> listOfCroppedImage = new ObservableCollection<Model_CroppedImage>();
         Bitmap copy;
@@ -66,6 +65,17 @@ namespace Proyecto_Detector_de_Leucemia.ViewModel
             get { return colorfilter; }
             set { colorfilter = value; OnPropertyChanged(); }
         }
+        public Model_CroppedImage ImageSelected
+        {
+            get { return imageSelected; }
+            set { imageSelected = value; OnPropertyChanged(); }
+        }
+        public Bitmap ImageMainBitmap
+        {
+            get { return imageMainBitmap; }
+            set { imageMainBitmap = value; OnPropertyChanged(); }
+        }
+
         #endregion
 
         #region Commands
@@ -345,8 +355,8 @@ namespace Proyecto_Detector_de_Leucemia.ViewModel
                             if (ImageSelected != null)
                             {
                                 
-                                ImageMain = ConvertImageGrayScale(ImageMain);
-                                imageToShow = ImageMain;
+                                ImageMain = ToBitmapImage(ConvertImageGrayScale(BitmapImageToBitmap(ImageMain)));
+                                ImageToShow = ImageMain;
                                 ImageSelected.HasChanged = true;
                                 RefreshImage();
                             }
@@ -359,6 +369,34 @@ namespace Proyecto_Detector_de_Leucemia.ViewModel
                 return applyGraysScaleImage;
             }
         }
+
+        private ICommand applyEqualizationImage;
+        public ICommand ApplyEqualizationImage
+        {
+            get
+            {
+                if (applyEqualizationImage == null)
+                    applyEqualizationImage = new RelayCommand(() =>
+                    {
+                        try
+                        {
+                            if (ImageSelected != null)
+                            {
+                                ImageMain = ToBitmapImage(ContrastStretchImage(BitmapImageToBitmap(ImageMain)));
+                                ImageToShow = ImageMain;
+                                ImageSelected.HasChanged = true;
+                                RefreshImage();
+                            }
+                        }catch(Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                        }
+                        
+                    });
+                return applyEqualizationImage;
+            }
+        }
+
         #endregion
 
         private ICommand saveChangesImage;
@@ -374,13 +412,163 @@ namespace Proyecto_Detector_de_Leucemia.ViewModel
                             //ImageSelected.HasChanged = true;
                             SaveChanges();
                         }
-                        catch (Exception)
+                        catch (Exception ex)
                         {
-
-                            throw;
+                            MessageBox.Show(ex.Message);
                         }
                     });
                 return saveChangesImage;
+            }
+        }
+
+        private ICommand applyLeukemiaConvertBN;
+        public ICommand ApplyLeukemiaConvertBN
+        {
+            get
+            {
+                if (applyLeukemiaConvertBN == null)
+                    applyLeukemiaConvertBN = new RelayCommand(() =>
+                    {
+                        try
+                        {
+                            if (ImageSelected != null)
+                            {
+                                ImageMainBitmap = ContrastStretchImage(BitmapImageToBitmap(ImageMain));
+                                ImageMainBitmap = GetPalette16Color(ImageMainBitmap);
+                                ImageMain = ToBitmapImage(ImageMainBitmap);
+                                ImageMain = RemoveBackgroundImage(ImageMain);
+                                ImageMainBitmap = ConvertImageGrayScale(BitmapImageToBitmap(ImageMain));
+                                ImageMainBitmap = Convert8bppFormatPixel(ImageMainBitmap);
+                                ImageMainBitmap = GetThreshold(ImageMainBitmap);
+                                ImageMainBitmap = GetInvertColor(ImageMainBitmap);
+                                ImageMain = ToBitmapImage(ImageMainBitmap);
+                                ImageToShow = ImageMain;
+                                ImageSelected.HasChanged = true;
+                                RefreshImage();
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                        }
+                    });
+                return applyLeukemiaConvertBN;
+            }
+        }
+
+        private ICommand applyLeukemiaDetectionImage;
+        public ICommand ApplyLeukemiaDetectionImage
+        {
+            get
+            {
+                if (applyLeukemiaDetectionImage == null)
+                    applyLeukemiaDetectionImage = new RelayCommand(() =>
+                    {
+                        try
+                        {
+                            if (ImageSelected != null)
+                            {
+                                ImageMainBitmap = BitmapImageToBitmap(imageMain);
+                                ImageMain = DetectionBlobsImage(ImageMainBitmap);
+                                ImageToShow = ImageMain;
+                                ImageSelected.HasChanged = true;
+                                RefreshImage();
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                        }
+                    });
+                return applyLeukemiaDetectionImage;
+            }
+        }
+
+        private ICommand applyErosionImage;
+        public ICommand ApplyErosionImage
+        {
+            get
+            {
+                if (applyErosionImage == null)
+                    applyErosionImage = new RelayCommand(() =>
+                    {
+                        try
+                        {
+                            if (ImageSelected != null)
+                            {
+                                ImageMainBitmap = BitmapImageToBitmap(ImageMain);
+                                if (ImageMainBitmap.PixelFormat != PixelFormat.Format8bppIndexed)
+                                    ImageMainBitmap = Convert8bppFormatPixel(ImageMainBitmap);
+                                ImageMainBitmap = ErosionImage(ImageMainBitmap);
+                                ImageMain = ToBitmapImage(ImageMainBitmap);
+                                ImageToShow = ImageMain;
+                                ImageSelected.HasChanged = true;
+                                RefreshImage();
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                        }
+                    });
+                return applyErosionImage;
+            }
+        }
+
+        private ICommand applyDilatationImage;
+        public ICommand ApplyDilatationImage
+        {
+            get
+            {
+                if (applyDilatationImage == null)
+                    applyDilatationImage = new RelayCommand(() =>
+                    {
+                        try
+                        {
+                            if (ImageSelected != null)
+                            {
+                                ImageMainBitmap = BitmapImageToBitmap(ImageMain);
+                                if (ImageMainBitmap.PixelFormat != PixelFormat.Format8bppIndexed)
+                                    ImageMainBitmap = Convert8bppFormatPixel(ImageMainBitmap);
+                                ImageMainBitmap = DilatationImage(ImageMainBitmap);
+                                ImageMain = ToBitmapImage(ImageMainBitmap);
+                                ImageToShow = ImageMain;
+                                ImageSelected.HasChanged = true;
+                                RefreshImage();
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                        }
+                    });
+                return applyDilatationImage;
+            }
+        }
+
+        private ICommand extractLeukemiaThreeImage;
+        public ICommand ExtractLeukemiaThreeImage
+        {
+            get
+            {
+                if (extractLeukemiaThreeImage == null)
+                    extractLeukemiaThreeImage = new RelayCommand(() =>
+                    {
+                        try
+                        {
+                            if (ImageSelected != null)
+                            {
+
+                                ExtractBlobsImage(ImageMain);
+
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                        }
+                    });
+                return extractLeukemiaThreeImage;
             }
         }
 
@@ -433,16 +621,17 @@ namespace Proyecto_Detector_de_Leucemia.ViewModel
         private BitmapImage RemoveBackgroundImage(BitmapImage imgProceseed)
         {
             ColorFilter = new EuclideanColorFiltering();
-            ColorFilter.CenterColor = new RGB(163, 57, 176);
+            ColorFilter.CenterColor = new RGB(163, 57, 176); //  191, 143, 145 216,182,186  205,160,116 B:163, 57, 176
             ColorFilter.Radius = 100;
+            ColorFilter.FillColor = new RGB(255, 255, 255);
             imgProceseed = ToBitmapImage(ColorFilter.Apply(BitmapImageToBitmap(imgProceseed)));
             return imgProceseed;
         }
 
-        private BitmapImage ConvertImageGrayScale(BitmapImage imgProceseed)
+        private Bitmap ConvertImageGrayScale(Bitmap imgProceseed)
         {
             Grayscale grayscale = new Grayscale(0.2125, 0.7154, 0.0721);
-            imgProceseed = ToBitmapImage(grayscale.Apply(BitmapImageToBitmap(imgProceseed)));
+            imgProceseed = grayscale.Apply(imgProceseed);
             return imgProceseed;
         }
 
@@ -468,6 +657,150 @@ namespace Proyecto_Detector_de_Leucemia.ViewModel
                 RefreshImage();
                 ImageSelected.HasChanged = false;
             }
+        }
+
+        //Es una tecnica que intenta mejorar el contraste en una imagen "estirando" el rango de valores
+        //Se diferencia de la ecualizacion en que solo puede aplicar una función de escala lineal a los valores
+        // de pixeles de imagen
+        private Bitmap ContrastStretchImage(Bitmap imgProceseed)
+        {
+            ContrastStretch filter = new ContrastStretch();
+            imgProceseed = filter.Apply(imgProceseed);
+            return imgProceseed;
+        }
+
+        private Bitmap GetPalette16Color(Bitmap imgProceseed)
+        {
+            ColorImageQuantizer ciq = new ColorImageQuantizer(new MedianCutQuantizer());
+            Color[] colorTable = ciq.CalculatePalette(imgProceseed, 16);
+
+            // ... or just reduce colors in the specified image
+            imgProceseed = ciq.ReduceColors(imgProceseed, colorTable);
+            return imgProceseed;
+        }
+
+        private Bitmap Convert8bppFormatPixel(Bitmap imgProceseed)
+        {
+            Rectangle rec = new Rectangle(0, 0, imgProceseed.Width, imgProceseed.Height);
+            imgProceseed = imgProceseed.Clone(rec, PixelFormat.Format8bppIndexed);
+            return imgProceseed;
+        }
+
+        private Bitmap GetThreshold(Bitmap imgProceseed)
+        {
+            Threshold threshold = new Threshold(100);
+            imgProceseed = threshold.Apply(imgProceseed);
+            return imgProceseed;
+        }
+
+        private Bitmap GetInvertColor(Bitmap imgProceseed)
+        {
+            Invert invert = new Invert();
+            imgProceseed = invert.Apply(imgProceseed);
+            return imgProceseed;
+        }
+
+        private Bitmap ErosionImage(Bitmap imgProceseed)
+        {
+            try{
+                Erosion erosion = new Erosion();
+                imgProceseed = erosion.Apply(imgProceseed);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("No cumple el formato de bpp necesario para este metodo");
+            }
+            return imgProceseed;
+        }
+
+        private Bitmap DilatationImage(Bitmap imgProceseed)
+        {
+            try
+            {
+                Dilatation dilatation = new Dilatation();
+                imgProceseed = dilatation.Apply(imgProceseed);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("No cumple el formato de bpp necesario para este metodo");
+            }
+            return imgProceseed;
+        }
+
+        private BitmapImage DetectionBlobsImage(Bitmap imgProceseed)
+        {
+            BlobCounter blobCounter = new BlobCounter();
+            blobCounter.ProcessImage(imgProceseed);
+            Blob[] blobs = blobCounter.GetObjectsInformation();
+
+            GrahamConvexHull hullFinder = new GrahamConvexHull();
+
+            BitmapData data = imgProceseed.LockBits(
+                new System.Drawing.Rectangle(0, 0, imgProceseed.Width, imgProceseed.Height),
+                    ImageLockMode.ReadWrite, imgProceseed.PixelFormat);
+
+            foreach (Blob blob in blobs)
+            {
+
+                List<IntPoint> edgePoints = new List<IntPoint>();
+                List<IntPoint> leftPoints, rightPoints;
+
+                blobCounter.GetBlobsLeftAndRightEdges(blob,
+                    out leftPoints, out rightPoints);
+
+                edgePoints.AddRange(leftPoints);
+                edgePoints.AddRange(rightPoints);
+
+                List<IntPoint> hull = hullFinder.FindHull(edgePoints);
+                Drawing.Polygon(data, hull, Color.Red);
+            }
+
+            imgProceseed.UnlockBits(data);
+            return ToBitmapImage(imgProceseed);
+        }
+        
+        private void ExtractBlobsImage(BitmapImage imgProceseed)
+        {
+            Random numR = new Random();
+
+            colorfilter = new EuclideanColorFiltering();
+            colorfilter.CenterColor = new RGB(163, 57, 176);
+            colorfilter.Radius = 100;
+            imgProceseed = ToBitmapImage(colorfilter.Apply(BitmapImageToBitmap(imgProceseed)));
+
+            BlobCounterBase bc = new BlobCounter();
+
+            bc.FilterBlobs = true;
+            bc.MinWidth = 15;
+            bc.MinHeight = 15;
+
+            bc.ObjectsOrder = ObjectsOrder.Size;
+
+            bc.ProcessImage(BitmapImageToBitmap(imgProceseed));
+            Blob[] blobs = bc.GetObjectsInformation();
+
+            UnmanagedImage[] unmanageds = new UnmanagedImage[3];
+            for (int i = 0; i < 3; i++)
+            {
+                int Rnum = numR.Next(1, 7);
+                if (blobs.Length > 0)
+                {
+                    bc.ExtractBlobsImage(BitmapImageToBitmap(imgProceseed), blobs[Rnum], true);
+                    unmanageds[i] = blobs[Rnum].Image;
+                }
+            }
+            foreach (var unmanaged in unmanageds)
+            {
+                ListOfCroppedImage.Add(new Model_CroppedImage { SourceImage = ExtractBiggestBlobImage(unmanaged.ToManagedImage()) });
+            }
+            
+
+        }
+        private BitmapImage ExtractBiggestBlobImage(Bitmap imgProceseed)
+        {
+            ExtractBiggestBlob filter = new ExtractBiggestBlob();
+            imgProceseed = filter.Apply(imgProceseed);
+            return ToBitmapImage(imgProceseed);
         }
 
         #endregion
